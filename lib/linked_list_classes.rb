66fcdcf1c7d68dcb0dc9f6437f36b0fc5b4329ad
sgibson47 <sgibson47@gmail.com>
2019-03-21 21:38:10 -0500

    add file to hold classes & require in methods file and spec file

---
+++ lib/linked_list_classes.rb
@@ -0,0 +1,8 @@
+# Definition for singly-linked list.
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val)
+        @val = val
+        @next = nil
+    end
+end
--- spec/lib/linked_list_methods_spec.rb
+++ spec/lib/linked_list_methods_spec.rb
@@ -1,2 +1,3 @@
 require 'spec_helper'
 require '/Users/samanthagibson/Development/code/leetcode-top-interview-questions-easy-linked-list-ruby/lib/linked_list_methods.rb'
+require '/Users/samanthagibson/Development/code/leetcode-top-interview-questions-easy-linked-list-ruby/lib/linked_list_classes.rb'
