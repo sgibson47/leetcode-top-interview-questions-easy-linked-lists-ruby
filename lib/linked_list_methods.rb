1362698520f7ddab05eb6bd0e9f944bc56bbbe47
sgibson47 <sgibson47@gmail.com>
2019-03-21 22:05:48 -0500

    add method to create a linked list from array

--- lib/linked_list_methods.rb
+++ lib/linked_list_methods.rb
@@ -1,1 +1,12 @@
 require './linked_list_classes'
+
+def make_linked_list(array)
+  i = 0 
+  last_node = nil
+  while i < array.length
+    new_node = ListNode.new(array[i])
+    new_node.next = last_node
+    last_node = new_node
+    i++
+  end
+end
